9b48e658c05c881c71fd19fb18c6ded7b1da0a18
PedroBZR <bzrpedro@gmail.com>
2025-11-17 20:30:15 -0300

    atualização do readme e arquivo de estilos globais

---
+++ frontend/plataformaAprendizado/styles/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+export const GlobalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "#f5f5f5",
+    },
+});
