725effdecd2ecc1714e233adcb317a2b5ce003cf
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2026-02-17 21:18:29 -0300

    criado gerador e validador de tokens jwt e também gerador de refreshToken
    para próximos commits pretendo desenvolver sistema de cadastro, login e correlatos com sistema de usuários

---
+++ backend/server_api/Services/Authentication/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using server_api.Context;
+
+namespace server_api.Services.Authentication
+{
+    public class AuthService
+    {
+        private readonly ContextDB _context;
+        private readonly JwtService _jwtService;
+        private readonly IConfiguration _configuration;
+        private readonly int _refreshTokenExpirationDays;
+
+        public AuthService(ContextDB context, JwtService jwtService, IConfiguration configuration)
+        {
+            _context = context;
+            _jwtService = jwtService;
+            _configuration = configuration;
+            _refreshTokenExpirationDays = int.Parse(_configuration["JwtSettings:RefreshTokenExpirationInDays"] ?? "7");
+        }
+        // fazer o sistema de login e autenticação
+    }
+}
