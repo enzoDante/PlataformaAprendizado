4cfa0f9c9e976172d78829dd31269dc0a7ce6d69
Amora <73441554+amoramm@users.noreply.github.com>
2026-08-05 20:31:25 -0300

    Add authorization to GameController

--- backend/server_api/Controllers/GameController.cs
+++ backend/server_api/Controllers/GameController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using server_api.Services.GameService;
 
 namespace server_api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class GameController : Controller
     {
+        private readonly GameUserService _gameUserService;
 
+        public GameController(GameUserService gameUserService)
+        {
+            _gameUserService = gameUserService;
+        }
+
+        [HttpPost("ClassCompleted/{classId}")]
+        public async Task<IActionResult> ClassCompleted(int classId)
+        {
+            var result = await _gameUserService.ClassCompleted(classId);
+            return Ok(result);
+        }
     }
 }
